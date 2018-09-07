5abd2996d77761737553c9b5a522dd7596433378
Sung J Byun <sungjbyun@D-10-19-122-32.dhcp4.washington.edu>
2018-09-06 18:31:38 -0700

    working on data fetching using axios, after this, might going back to yelp app and re do it/update

--- src/App.js
+++ src/App.js
@@ -45,42 +45,70 @@
 
 // export default App;
 
-import React, { Component } from 'react'
-import Todos from './Todos';
-import AddTodo from './AddForm';
+// made a simple todo list project
+// import React, { Component } from 'react'
+// import Todos from './Todos';
+// import AddTodo from './AddForm';
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, content: 'Hit the gym'},
-      {id: 2, content: 'Do grocery shopping'},
-    ]
-  }
-  deleteTodo = (id) => {
-    // console.log(id)
-    let todos = this.state.todos.filter(todo => {
-      return todo.id !== id // its gonna be true or false
-    });
-    this.setState({
-      todos
-    })
-  }
-  addTodo = (todo) => {
-    // console.log(id)
-    todo.id = Math.random();
-    let todos = [...this.state.todos, todo]
-    this.setState({
-      todos
-    })
-  }
-  render() {
+// export default class App extends Component {
+//   state = {
+//     todos: [
+//       {id: 1, content: 'Hit the gym'},
+//       {id: 2, content: 'Do grocery shopping'},
+//     ]
+//   }
+//   deleteTodo = (id) => {
+//     // console.log(id)
+//     let todos = this.state.todos.filter(todo => {
+//       return todo.id !== id // its gonna be true or false
+//     });
+//     this.setState({
+//       todos
+//     })
+//   }
+//   addTodo = (todo) => {
+//     // console.log(id)
+//     todo.id = Math.random();
+//     let todos = [...this.state.todos, todo]
+//     this.setState({
+//       todos
+//     })
+//   }
+//   render() {
+//     return (
+//       <div className='todo-app container'>
+//         <h1 className='center blue-text'>Todo's</h1>
+//         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+//         <AddTodo addTodo={this.addTodo}/>
+//       </div>
+//     )
+//   }
+// }
+
+// excercise with react router
+import React, { Component } from 'react'
+import {BrowserRouter, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Post from './components/Post';
+
+class App extends Component {
+  render () {
     return (
-      <div className='todo-app container'>
-        <h1 className='center blue-text'>Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-        <AddTodo addTodo={this.addTodo}/>
-      </div>
+      <BrowserRouter>
+        <div className='App'>
+          <NavBar />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/:post_id' component={Post} />
+        </div>
+      </BrowserRouter>
+
     )
   }
 }
 
+export default App
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className='container'>
+      <h4 className='center'>About</h4>
+      <p>AboutAboutAboutAbout</p>
+    </div>
+  )
+}
