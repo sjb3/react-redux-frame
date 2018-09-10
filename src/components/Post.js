44a97c066caa8ed94d8e0c599e00aa4887818d33
Sung J Byun <sungjbyun@D-10-19-233-230.dhcp4.washington.edu>
2018-09-09 20:28:16 -0700

    finishing up each page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
+const endPoint = 'https://jsonplaceholder.typicode.com/';
 
 class Post extends Component {
   state = {
-    id: null
+    post: null
   }
   componentDidMount() {
     // console.log('>>>>',this.props);
     let id = this.props.match.params.post_id;
-    this.setState({
-      id
-    })
+    axios.get(endPoint + 'posts/' + id)
+      .then(res => {
+        this.setState({
+          post: res.data
+        })
+      })
   }
   render () {
+    const post = this.state.post ?
+    (<div className='post'>
+      <h4 className='center'>{this.state.post.title}</h4>
+      <p>{this.state.post.body}</p>
+    </div>) :
+    (<div className='center'>Loading ...</div>)
+
     return (
       <div className='container'>
-        <h4>{this.state.id}</h4>
+        {post}
       </div>
     )
   }
